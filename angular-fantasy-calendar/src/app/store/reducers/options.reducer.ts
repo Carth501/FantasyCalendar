59266b9fa45b89b6bfd6aba01d6a200278635bc4
Carth501 <carth501@gmail.com>
2020-05-19 18:04:13 -0500

    Fixed critical mistake in the reducer

--- angular-fantasy-calendar/src/app/store/reducers/options.reducer.ts
+++ angular-fantasy-calendar/src/app/store/reducers/options.reducer.ts
@@ -13,11 +13,16 @@ export const initialState: State = {
 const optionsReducer = createReducer(
     initialState,
     on(OptionsActions.setDirtyCalendar, (state, {calendar}) => ({ ...state, dirtySettings: calendar})),
-    on(OptionsActions.setYearMath, (state, {yearMath}) => ({ ...state, yearMath})),
-    on(OptionsActions.setEventLists, (state, {eventLists}) => ({ ...state, eventLists})),
-    on(OptionsActions.setLeapYears, (state, {leapYears}) => ({ ...state, leapYears})),
-    on(OptionsActions.setMonths, (state, {settingsMonths}) => ({ ...state, settingsMonths})),
-    on(OptionsActions.setDoW, (state, {DoW}) => ({ ...state, DoW}))
+    on(OptionsActions.setYearMath, (state, {yearMath}) => ({ ...state, dirtySettings:
+        {...state.dirtySettings, year: yearMath }})),
+    on(OptionsActions.setEventLists, (state, {eventLists}) => ({ ...state, dirtySettings:
+        {...state.dirtySettings, eventLists}})),
+    on(OptionsActions.setLeapYears, (state, {leapYears}) => ({ ...state, dirtySettings:
+        {...state.dirtySettings, leapYears}})),
+    on(OptionsActions.setMonths, (state, {settingsMonths}) => ({ ...state, dirtySettings:
+        {...state.dirtySettings, settingsMonths}})),
+    on(OptionsActions.setDoW, (state, {DoW}) => ({ ...state, dirtySettings:
+        {...state.dirtySettings, DoW}}))
 );
 
 export function reducer(state: State | undefined, action: Action) {

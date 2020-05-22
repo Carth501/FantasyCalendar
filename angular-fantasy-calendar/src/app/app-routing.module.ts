06e70fdeca97b11850c926e655da28ea6bc30b8f
Carth501 <carth501@gmail.com>
2020-05-22 14:30:43 -0500

    trying to route all urls to calendar

--- angular-fantasy-calendar/src/app/app-routing.module.ts
+++ angular-fantasy-calendar/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ViewComponent } from './components/view/view.component';
 
 const routes: Routes = [
   { path: 'calendar', component: ViewComponent },
-  { path: '', redirectTo: '/calendar', pathMatch: 'full'}
+  { path: '*', redirectTo: '/calendar', pathMatch: 'full'}
 ];
 
 @NgModule({
